525764091b079d0c3e44c8167676f5358c9f56d4
shazgoran241-alt <shazgoran241@gmail.com>
2025-11-09 16:35:30 +0300

    Add files via upload

---
+++ shazQ1/src/shazq1/ShazQ1.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package shazq1;
+import java.util.Scanner;
+public class ShazQ1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char x = 0;
+        while(x<5){
+            System.out.println("* * * * * * ");
+            x++;
+            System.out.println("+ + + + + +");
+            // mamosta gyan lera bo dubara bunaway sumbolakan while bakar dahenen
+            //am x++ sh boya danusen agar naynusen ba be kota * w+ daanuse 
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
